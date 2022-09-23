203952f40d653da185d59342077f53251b5f25e8
Hasan <hhasanyurdakul@gmail.com>
2022-09-23 15:56:54 +0300

    Init

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+export { default as ChannelDetail } from "./ChannelDetail.jsx";
+export { default as Feed } from "./Feed";
+export { default as Navbar } from "./Navbar.jsx";
+export { default as SearchFeed } from "./SearchFeed.jsx";
+export { default as VideoDetail } from "./VideoDetail.jsx";
+export { default as SearchBar } from "./SearchBar";
+export { default as Sidebar } from "./Sidebar";
+export { default as Videos } from "./Videos";
+export { default as VideoCard } from "./VideoCard";
+export { default as ChannelCard } from "./ChannelCard";
---
+++ src/utils/constants.js
@@ -0,0 +1,39 @@
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import HomeIcon from "@mui/icons-material/Home";
+import CodeIcon from "@mui/icons-material/Code";
+import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import LiveTvIcon from "@mui/icons-material/LiveTv";
+import SchoolIcon from "@mui/icons-material/School";
+import FaceRetouchingNaturalIcon from "@mui/icons-material/FaceRetouchingNatural";
+import CheckroomIcon from "@mui/icons-material/Checkroom";
+import GraphicEqIcon from "@mui/icons-material/GraphicEq";
+import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
+
+export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
+
+export const categories = [
+  { name: "New", icon: <HomeIcon /> },
+  { name: "Coding", icon: <CodeIcon /> },
+  { name: "Music", icon: <MusicNoteIcon /> },
+  { name: "Education", icon: <SchoolIcon /> },
+  { name: "Podcast", icon: <GraphicEqIcon /> },
+  { name: "Movie", icon: <OndemandVideoIcon /> },
+  { name: "Gaming", icon: <SportsEsportsIcon /> },
+  { name: "Sport", icon: <FitnessCenterIcon /> },
+  { name: "Fashion", icon: <CheckroomIcon /> },
+  { name: "Beauty", icon: <FaceRetouchingNaturalIcon /> },
+  { name: "Comedy", icon: <TheaterComedyIcon /> },
+  { name: "Gym", icon: <FitnessCenterIcon /> },
+  { name: "Crypto", icon: <DeveloperModeIcon /> },
+];
+
+export const demoThumbnailUrl =
+  "https://images.unsplash.com/photo-1663875972135-bb6654ed702d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80";
+export const demoChannelUrl = "/channel/hasanyurdakul";
+export const demoVideoUrl = "/video/hasanyurdakul";
+export const demoChannelTitle = "Hasan Yurdakul";
+export const demoVideoTitle = "Hasan Yurdakul";
+export const demoProfilePicture = "https://roilenergy.com/img/icons/avatar.jpg";
